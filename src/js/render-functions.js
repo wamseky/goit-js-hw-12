c9e44c283478e24ac578f4602c90699d0bc2c3b9
Diana Kharchenko <vitadiana2@gmail.com>
2024-03-11 18:38:21 +0200

    .

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,30 +1,8 @@
-// export function renderMarkup(data) {
-//     return data.hits
-//       .map(
-//         element => `<div class="gallery-item">
-//         <a class="gallery-link" href="${element.largeImageURL}">
-//             <img class="gallery-image" src="${element.webformatURL}" alt="${element.tags}">
-//         </a>
-//         <div class="gallery-image-info">
-//             <p class="image-info-item"><span class="image-items-text">Likes: </span>${element.likes}</p>
-//             <p class="image-info-item"><span class="image-items-text">Views: </span>${element.views}</p>
-//             <p class="image-info-item"><span class="image-items-text">Comments: </span>${element.comments}</p>
-//             <p class="image-info-item"><span class="image-items-text">Downloads: </span>${element.downloads}</p>
-//         </div>
-//     </div>`
-//       )
-//       .join('')
-    
-//   }
-
-
-
-
 export function renderGallery(images) {
-    return images
-      .map(
-        img =>
-          `
+  return images
+    .map(
+      img =>
+        `
           <li class="gallery-item">
             <div class="gallery-box item-card-wrapper">
               <a class="gallery-link" href="${img.largeImageURL}">
@@ -50,6 +28,6 @@ export function renderGallery(images) {
               </div>
             </div>
           </li>`
-      )
-      .join('');
-  }
+    )
+    .join('');
+}
