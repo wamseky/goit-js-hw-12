1084d6b03aaf61e845760541d1ab4f7de22e996d
Diana Kharchenko <140257778+wamseky@users.noreply.github.com>
2024-03-12 16:03:19 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -52,19 +52,7 @@ async function fetchRequest(event) {
         timeout: 3000,
       });
     }
-    if (images.length === 0) {
-      iziToast.error({
-        theme: 'dark',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        messageColor: '#ffffff',
-        backgroundColor: '#ef4040',
-        position: 'topRight',
-        pauseOnHover: false,
-        progressBarColor: '#b51b1b',
-        timeout: 3000,
-      });
-    }
+
 
     refs.gallery.innerHTML = renderGallery(images);
 
